4ccdf7f2aece7266e758ca8f1fcf9700353fbc41
gabi <130366455+gabigringavolei@users.noreply.github.com>
2024-10-24 11:51:07 +0000

    middleware da  pasta madle + css menu

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3300;
+const PORT = 3303;
 
-app.get("/", function (re, res) {
-  res.send("Ola!.....");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/views/index.html");
 });
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+// ROTA UTILIZANDO MIDDLEWARE(404)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
 app.listen(PORT, () => {
   console.log("running...");
 });
